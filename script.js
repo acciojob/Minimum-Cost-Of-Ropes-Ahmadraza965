fa8abbc6af25441c696e88324a144eaae9ee741c
Ahmadraza965 <120107650+Ahmadraza965@users.noreply.github.com>
2023-07-05 18:51:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-function calculateMinCost() {
-  //your code here
-  
-  
-  
-}  
+function findMinimumCost(ropeLengths) {
+    const minHeap = new MinHeap();
+
+    ropeLengths.forEach(length => minHeap.insert(length));
+
+    const totalCost = ropeLengths.reduce((acc, _, index) => {
+        if (minHeap.size() > 1) {
+            const rope1 = minHeap.extractMin();
+            const rope2 = minHeap.extractMin();
+            const cost = rope1 + rope2;
+            minHeap.insert(cost);
+            return acc + cost;
+        }
+        return acc;
+    }, 0);
+
+    return totalCost;
+}
